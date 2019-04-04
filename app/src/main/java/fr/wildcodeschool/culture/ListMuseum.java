c1b5d5348f51f58246f500d653792ac6ab95a223
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-04-04 14:56:33 +0200

    ListView a terminer

--- app/src/main/java/fr/wildcodeschool/culture/ListMuseum.java
+++ app/src/main/java/fr/wildcodeschool/culture/ListMuseum.java
@@ -2,6 +2,14 @@ package fr.wildcodeschool.culture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class ListMuseum extends AppCompatActivity {
 
@@ -9,5 +17,45 @@ public class ListMuseum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_museum);
+
+        String json = null;
+
+        try {
+            InputStream is = getAssets().open("Toulouse-musees.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            JSONArray root = new JSONArray(json);
+            for (int i = 0; i < root.length(); i++) {
+                JSONObject rooter = (JSONObject) root.get(i);
+                JSONObject fields = rooter.getJSONObject("fields");
+                for (int b = 0; b < fields.length(); b++){
+                    String name = (String) fields.get("eq_nom_equipement");
+                    String numero = (String) fields.get("eq_telephone");
+                    String horaires = (String) fields.get("eq_horaires");
+                    String site = (String) fields.get("eq_site_web");
+                    String metro =  (String) fields.get("eq_acces_metro");
+
+                    Museum musées = new Museum(name,numero,horaires,site,metro);
+
+                    List<Museum> menu = Arrays.asList(musées);
+
+                    ListView listMenu = findViewById(R.id.listView);
+                    ListMuseumAdapter adapter = new ListMuseumAdapter(ListMuseum.this, menu);
+                    listMenu.setAdapter(adapter);
+
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/fr/wildcodeschool/culture/Museum.java
+++ app/src/main/java/fr/wildcodeschool/culture/Museum.java
@@ -1,15 +1,20 @@
 package fr.wildcodeschool.culture;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Museum implements Parcelable {
+public class Museum {
 
     String name;
-    int numero;
+    String numero;
     String horaires;
     String site;
     String metro;
+
+    public Museum(String name, String numero, String horaires, String site, String metro) {
+        this.name = name;
+        this.numero = numero;
+        this.horaires = horaires;
+        this.site = site;
+        this.metro = metro;
+    }
 
     public String getName() {
         return name;
@@ -19,11 +24,11 @@ public class Museum implements Parcelable {
         this.name = name;
     }
 
-    public int getNumero() {
+    public String getNumero() {
         return numero;
     }
 
-    public void setNumero(int numero) {
+    public void setNumero(String numero) {
         this.numero = numero;
     }
 
@@ -51,43 +56,7 @@ public class Museum implements Parcelable {
         this.metro = metro;
     }
 
-    public static Creator<Museum> getCREATOR() {
-        return CREATOR;
-    }
 
 
-    protected Museum(Parcel in) {
-        name = in.readString();
-        numero = in.readInt();
-        horaires = in.readString();
-        site = in.readString();
-        metro = in.readString();
-    }
-
-    public static final Creator<Museum> CREATOR = new Creator<Museum>() {
-        @Override
-        public Museum createFromParcel(Parcel in) {
-            return new Museum(in);
-        }
-
-        @Override
-        public Museum[] newArray(int size) {
-            return new Museum[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeInt(numero);
-        dest.writeString(horaires);
-        dest.writeString(site);
-        dest.writeString(metro);
-    }
 }
 
