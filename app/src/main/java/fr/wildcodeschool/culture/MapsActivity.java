7a335ab81ad83e7edf694d91072faeb1ae4a3770
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-04-11 11:05:19 +0200

    création de l'activité Sign In

--- app/src/main/java/fr/wildcodeschool/culture/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/culture/MapsActivity.java
@@ -4,14 +4,13 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,6 +18,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.transitionseverywhere.TransitionManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,14 +27,13 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import com.transitionseverywhere.TransitionManager;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    FloatingActionButton btFavorite, btBurger, btPlaces;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1550;
+    FloatingActionButton btFavorite, btBurger, btPlaces, btSignOut;
     CoordinatorLayout transitionContainer;
     private GoogleMap mMap;
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1550;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,8 +80,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        }
-        else{
+        } else {
             mMap.setMyLocationEnabled(true);
         }
         String json = null;
@@ -102,13 +101,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             for (int i = 0; i < root.length(); i++) {
                 JSONObject rooter = (JSONObject) root.get(i);
                 JSONObject fields = rooter.getJSONObject("fields");
-                for (int b = 0; b < fields.length(); b++){
-                    JSONArray geolocalisation =(JSONArray) fields.get("geo_point_2d");
+                for (int b = 0; b < fields.length(); b++) {
+                    JSONArray geolocalisation = (JSONArray) fields.get("geo_point_2d");
                     String name = (String) fields.get("eq_nom_equipement");
                     Double latitude = (Double) geolocalisation.get(0);
                     Double longitude = (Double) geolocalisation.get(1);
 
-                    LatLng museum = new LatLng(latitude,longitude);
+                    LatLng museum = new LatLng(latitude, longitude);
 
                     mMap.addMarker(new MarkerOptions().position(museum).title(name));
                 }
@@ -128,10 +127,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         btBurger = (FloatingActionButton) transitionContainer.findViewById(R.id.floatingActionButton);
         btFavorite = (FloatingActionButton) transitionContainer.findViewById(R.id.floatingFavoriteBt);
         btPlaces = (FloatingActionButton) transitionContainer.findViewById(R.id.floatingListPlaces);
+        btSignOut = (FloatingActionButton) transitionContainer.findViewById(R.id.floatingSignOut);
 
         btBurger.setOnClickListener(new View.OnClickListener() {
 
             int i = 0;
+
             @SuppressLint("RestrictedApi")
             @Override
             public void onClick(View v) {
@@ -140,12 +141,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     TransitionManager.beginDelayedTransition(transitionContainer);
                     btFavorite.setVisibility(View.VISIBLE);
                     btPlaces.setVisibility(View.VISIBLE);
+                    btSignOut.setVisibility(View.VISIBLE);
                     i++;
                 } else if (i == 1) {
 
                     TransitionManager.beginDelayedTransition(transitionContainer);
                     btFavorite.setVisibility(View.GONE);
                     btPlaces.setVisibility(View.GONE);
+                    btSignOut.setVisibility(View.GONE);
                     i = 0;
                 }
             }
@@ -156,6 +159,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View v) {
                 Intent gotoMain = new Intent(MapsActivity.this, MainActivity.class);
                 startActivity(gotoMain);
+
+            }
+        });
+
+        btSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(MapsActivity.this, SignIn.class));
             }
         });
     }
