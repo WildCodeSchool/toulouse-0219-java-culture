2e7ebe04a133d8fb2756e53ef2870b87b4dc5dc1
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-04-17 10:58:25 +0200

    Add favorites

--- app/src/main/java/fr/wildcodeschool/culture/FavoritesActivity.java
+++ app/src/main/java/fr/wildcodeschool/culture/FavoritesActivity.java
@@ -5,23 +5,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FavoritesActivity extends AppCompatActivity {
-    String Tag="Test";
-    ListView maListe;
-    List<Museum> favorites = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
-        Button favorites
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("favorites");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String favorite = (String) dataSnapshot.getValue();
+                Toast.makeText(FavoritesActivity.this, favorite,
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
 
     }
 
--- app/src/main/java/fr/wildcodeschool/culture/ListMuseum.java
+++ app/src/main/java/fr/wildcodeschool/culture/ListMuseum.java
@@ -2,13 +2,11 @@ package fr.wildcodeschool.culture;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.transitionseverywhere.TransitionManager;
@@ -39,7 +37,6 @@ public class ListMuseum extends AppCompatActivity {
             }
         });
     }
-
 
     // Creation Menu Flottant
     public void floatingMenu() {
