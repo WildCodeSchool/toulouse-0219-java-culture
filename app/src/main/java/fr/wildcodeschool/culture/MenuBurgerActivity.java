543bffbc277b14a8572f1922afe11d8cf4188901
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-04-30 10:59:13 +0200

    Nothing

--- app/src/main/java/fr/wildcodeschool/culture/MenuBurgerActivity.java
+++ app/src/main/java/fr/wildcodeschool/culture/MenuBurgerActivity.java
@@ -1,26 +1,24 @@
 package fr.wildcodeschool.culture;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.support.annotation.RestrictTo;
+import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.transitionseverywhere.TransitionManager;
 
 public class MenuBurgerActivity extends AppCompatActivity {
-        FloatingActionButton btFavorite, btBurger, btPlaces;
-        CoordinatorLayout transitionContainer;
-        @Override
-        protected void onCreate (Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu_burger);
-            floatingMenu();
-        }
+    FloatingActionButton btFavorite, btBurger, btPlaces;
+    CoordinatorLayout transitionContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_burger);
+        floatingMenu();
+    }
 
     // Creation Menu
     public void floatingMenu() {
@@ -33,6 +31,7 @@ public class MenuBurgerActivity extends AppCompatActivity {
         btBurger.setOnClickListener(new View.OnClickListener() {
 
             int i = 0;
+
             @SuppressLint("RestrictedApi")
             @Override
             public void onClick(View v) {
