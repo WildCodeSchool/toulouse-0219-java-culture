0dd61ddadabfbd7b8601fe5d18305a50a5a4020b
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-04-30 13:34:14 +0200

    Pr

--- app/src/main/java/fr/wildcodeschool/culture/ListMuseumAdapter.java
+++ app/src/main/java/fr/wildcodeschool/culture/ListMuseumAdapter.java
@@ -47,7 +47,7 @@ public class ListMuseumAdapter extends ArrayAdapter<Museum> {
         numero.setText(museum.getNumero());
         horaires.setText(museum.getHoraires());
         site.setText(museum.getSite());
-        distance.setText(Float.toString(museum.getDistance())+"  m");
+        distance.setText(String.format("%s m", Float.toString(museum.getDistance())));
         metro.setText(museum.getMetro());
 
         favorite.setOnClickListener(new View.OnClickListener() {
